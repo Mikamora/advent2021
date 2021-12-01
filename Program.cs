99472e86a651e3e1a90fe349813e3728da8b1cf1
Michael Richardson <mor7991@yahoo.com>
2021-12-01 00:03:35 -0600

    day1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sonar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isGreater = 0;
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Mikamora\Documents\JS\advent\advent-1-input.txt");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i + 3 < lines.Length)
+                {
+                    int measure1 = Int32.Parse(lines[i]) + Int32.Parse(lines[i + 1]) + Int32.Parse(lines[i + 2]);
+                    int measure2 = Int32.Parse(lines[i + 1]) + Int32.Parse(lines[i + 2]) + Int32.Parse(lines[i + 3]);
+                    if (measure1 < measure2)
+                    {
+                        isGreater = isGreater + 1;
+                    }
+
+                }
+
+                else
+                {
+                    Console.WriteLine(isGreater);
+                    return;
+                }
+            }
+        }
+    }
+}
